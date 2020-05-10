0ac464e8319c6e05a75a35ac8703de449b61f348
Sualeh Ali <smsali97@gmail.com>
2020-05-11 01:41:26 +0500

    Added build for windows

---
+++ client/build/precache-manifest.d7cf145e0df93c5b36c833f4ac1b00d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9cb8465c6caf30fcceab577263d7d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72cf58a18235bf83b0f8",
+    "url": "/static/css/main.5cc8b9f1.chunk.css"
+  },
+  {
+    "revision": "e641ff982d05bc815e87",
+    "url": "/static/js/2.be8316c1.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/static/js/2.be8316c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72cf58a18235bf83b0f8",
+    "url": "/static/js/main.9118f9d4.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
